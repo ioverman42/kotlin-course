28e9e5f7770c3cd6d44812880473d462ecf798c9
Isabel <iaoverman@madisoncollege.edu>
2023-06-07 14:49:31 -0500

    feeds the fish pellets every random day

--- src/main/kotlin/AMS.kt
+++ src/main/kotlin/AMS.kt
@@ -3,15 +3,32 @@ import java.util.*
 fun main(args: Array<String>) {
     println("Hello ${args[0]}!")
 
+    feedTheFish()
+
+
     //dayOfWeek()
 
-    timeGreeting("12")
+    //timeGreeting("12")
     //suggested way of doing timeGreeting on the main()
     /*
     if (args[0].toInt() < 12) println("Good morning")
     else println("Good night" )
      */
 }
+
+fun feedTheFish() {
+    val day = randomDay()
+    val food = "pellets"
+    println("Today is $day and the fish eat $food")
+}
+
+fun randomDay() : String {
+    val week = listOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday") //weekdays
+    return week[Random().nextInt(7)] //gets random number 0-6 (up to not including 7), and returns associated weekday
+}
+
+
+
 
 fun dayOfWeek() {
     println("What day of the week is it today?")
