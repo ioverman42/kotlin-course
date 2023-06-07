40aca041de55985fd74ed10a50cd7a6b608a386f
Isabel <iaoverman@madisoncollege.edu>
2023-06-07 14:39:04 -0500

    timeGreeting added and completed

--- src/main/kotlin/AMS.kt
+++ src/main/kotlin/AMS.kt
@@ -1,22 +1,39 @@
 import java.util.*
 
-          fun main (args: Array<String>) {
-              println("Hello ${args[0]}!")
-              //dayOfWeek()
-          }
+fun main(args: Array<String>) {
+    println("Hello ${args[0]}!")
 
-          fun dayOfWeek() {
-              println("What day of the week is it today?")
-              val weekday = Calendar.getInstance().get(Calendar.DAY_OF_WEEK)
-              println( when (weekday) {
-                  1 -> "Today is Sunday"
-                  2 -> "Today is Monday"
-                  3 -> "Today is Tuesday"
-                  4 -> "Today is Wednesday"
-                  5 -> "Today is Thursday"
-                  6 -> "Today is Friday"
-                  7 -> "Today is Saturday"
-                  else -> "Congratulations you have broken time"
-              })
-          }
+    //dayOfWeek()
 
+    timeGreeting("12")
+    //suggested way of doing timeGreeting on the main()
+    /*
+    if (args[0].toInt() < 12) println("Good morning")
+    else println("Good night" )
+     */
+}
+
+fun dayOfWeek() {
+    println("What day of the week is it today?")
+    val weekday = Calendar.getInstance().get(Calendar.DAY_OF_WEEK)
+    println(
+        when (weekday) {
+            1 -> "Today is Sunday"
+            2 -> "Today is Monday"
+            3 -> "Today is Tuesday"
+            4 -> "Today is Wednesday"
+            5 -> "Today is Thursday"
+            6 -> "Today is Friday"
+            7 -> "Today is Saturday"
+            else -> "Congratulations you have broken time"
+        }
+    )
+}
+
+fun timeGreeting(hour: String) {
+    if (hour.toInt() < 12) {
+        println("Good morning")
+    } else if (hour.toInt() >= 12) {
+        println("Good evening")
+    }
+}
