2d23a24c48ec738cc64fbf1ef986c556e8e80b88
Isabel <isabel.overman@gmail.com>
2023-06-17 22:26:38 -0500

    moved getBirthday into separate method

--- src/main/kotlin/FortuneCookie.kt
+++ src/main/kotlin/FortuneCookie.kt
@@ -1,12 +1,29 @@
 fun main(args: Array<String>) {
-    println(getFortuneCookie())
+    println(getFortuneCookie(getBirthday()))
 }
 
-fun getFortuneCookie() : String {
+fun getFortuneCookie(userBirthday : Int) : String {
     var fortunes = listOf("They're watching you...", "Terrible, terrible shit is about to come your way",
             "Dead looks good on you", "You laugh now, wait until you get home", "Pick another fortune cookie")
-    print("Enter your birthday: ")
-    var userBirthday = readLine()
-    var remainder = (userBirthday?.toIntOrNull() ?: 1) % 5
-    return("Fortune: ${fortunes[remainder]}")
+    val pattern = "0229....".toRegex() //using Regex here could result in some cool shit e.g. any Feb 29th birthday...
+    val remainder = (userBirthday) % 5
+    return when (userBirthday) {
+                        10301998 -> "Fortune: Hey, Isabel! You're the best!"
+                        else -> "Fortune: ${fortunes[remainder]}"
+                    }
 }
+
+fun getBirthday() : Int {
+    print("Enter your birthday [MMDDYYYY]: ")
+    return readLine()?.toIntOrNull() ?: 1
+}
+
+/* //Here's another way to do this
+fun main(args: Array<String>) {
+    var fortune: String for (i in 1..10) {
+        fortune = getFortune(getBirthday())
+        println("\nYour fortune is: $fortune")
+        if (fortune.contains("Take it easy")) break;
+    }
+}
+*/
